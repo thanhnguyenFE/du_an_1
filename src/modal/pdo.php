09b146fc31923f0eacb775120c8af0dbca556586
thanhnx <thanhnx@vega.com.vn>
2023-11-21 08:41:25 +0700

    'thanhnx'

---
+++ src/modal/pdo.php
@@ -0,0 +1,67 @@
+<?php
+function pdo_get_connection(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=duanmau2023", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+}
+//Thực hiện câu lệnh sql thao tác dữ liệu như (insert, update, delete)
+function pdo_execute($sql){
+    $sql_args=array_slice(func_get_args(),1);
+    try{
+        $conn=pdo_get_connection();
+        $stmt=$conn->prepare($sql);
+        $stmt->execute($sql_args);
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($conn);
+    }
+}
+// truy vấn nhiều dữ liệu - thực thi câu lệnh sql truy vấn (select * from tên bảng) => trả về nhiều bản ghi
+function pdo_query($sql){
+    $sql_args=array_slice(func_get_args(),1);
+
+    try{
+        $conn=pdo_get_connection();
+        $stmt=$conn->prepare($sql);
+        $stmt->execute($sql_args);
+        $rows=$stmt->fetchAll();
+        return $rows;
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($conn);
+    }
+}
+// truy vấn  1 dữ liệu
+// Thực thi câu lệnh sql truy vấn dữ lệnh (select  * from tên bảng where id/ma/.. = ..) => trả về 1 bản ghi
+function pdo_query_one($sql){
+    $sql_args=array_slice(func_get_args(),1);
+    try{
+        $conn=pdo_get_connection();
+        $stmt=$conn->prepare($sql);
+        $stmt->execute($sql_args);
+        $row=$stmt->fetch(PDO::FETCH_ASSOC);
+        // đọc và hiển thị giá trị trong danh sách trả về
+        return $row;
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($conn);
+    }
+}
+pdo_get_connection();
+?>
