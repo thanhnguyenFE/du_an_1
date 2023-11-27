0f17b815efc0e44fe4b6719244c8086b8eff5f3e
vietdq <vdinh4356@gmail.com>
2023-11-27 10:06:13 +0700

    vietdq

---
+++ src/view/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="../dist/output.css" rel="stylesheet">
+    <link rel="stylesheet" href="../dist/du_an_1.css">
+</head>
+<body>
+    <section class="container">
+        <header class="header">
+            <section class="logo">
+            <img src="../img/Cineworld-logo-1200x800-1.jpg" alt="" >
+            </section>
+        <nav> 
+            <a href="#">Trang chủ</a>
+            <a href="#">Lịch chiếu</a>
+            <a href="#">Tin tức</a>
+            <a href="#">Khuyến mãi </a>
+            <a href="#">Giá vé</a>
+            <a href="#">Liên hoan phim</a>
+            <a href="#">Giới thiệu</a>
+        </nav>
+        <section class="text">
+        <button class="dangki">Đăng kí </button>
+    </section>
+    <section class="text1">
+        <button class="dangnhap">Đăng nhập </button>
+    </section>
+    
+    </section> 
+    
+    </header>
+    <script src="main.js">
+
+</script>
